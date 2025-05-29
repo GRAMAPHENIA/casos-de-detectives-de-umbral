cce4247e8f13ad108c8c13c466ba4dcecf2f7ae8
gramaphenia <dicoratojuanpablo@gmail.com>
2025-05-29 19:51:03 -0300

    feat: agregar tooltips al menú y notificaciones para mejorar la accesibilidad

--- src/components/header/menu/menu.tsx
+++ src/components/header/menu/menu.tsx
@@ -1,18 +1,25 @@
 import Image from "next/image";
 import Notifications from "../notification/Notifications";
+import { Tooltip } from "@nextui-org/tooltip";
 
 export default function Menu() {
   return (
     <nav className="p-2 m-2 mt-3 flex justify-between">
       <ul>
         <li>
-          <Image
-            className="invert-25 hover:invert-[1] transition-all cursor-pointer"
-            src="/icons/menu.svg"
-            alt="Menu"
-            width={24}
-            height={24}
-          />
+          <Tooltip
+            content="Menú"
+            placement="right-start"
+            className="bg-zinc-900 px-4 rounded-lg text-zinc-400"
+          >
+            <Image
+              className="invert-25 hover:invert-[1] transition-all cursor-pointer"
+              src="/icons/menu.svg"
+              alt="Menu"
+              width={24}
+              height={24}
+            />
+          </Tooltip>
         </li>
       </ul>
       <ul className="flex gap-4">
--- src/components/header/notification/Notifications.tsx
+++ src/components/header/notification/Notifications.tsx
@@ -1,13 +1,23 @@
 import Image from "next/image";
+import { Tooltip } from "@nextui-org/tooltip";
 
 export default function Notifications() {
   return (
-    <Image
-      className="invert-25 hover:invert-[1] transition-all cursor-pointer"
-      src="/icons/bell-notification.svg"
-      alt="Bell"
-      width={24}
-      height={24}
-    />
+    <>
+      <Tooltip
+        content="Notificaciones"
+        placement="right-start"
+        className="bg-zinc-900 px-4 rounded-lg text-zinc-400"
+      >
+        <Image
+        className="invert-25 hover:invert-[1] transition-all cursor-pointer"
+        src="/icons/bell-notification.svg"
+        alt="Bell"
+        width={24}
+        height={24}
+      />
+      </Tooltip>
+    </>
+    
   );
 }
